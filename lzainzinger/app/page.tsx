a54200d4c9811da5cb97311565c587a68f60e73c
Zainzinger, Lukas (SI RSS EMEA SV DSC) <lukas.zainzinger@siemens.com>
2024-06-03 13:41:59 +0200

    added projects and photo portfolio placeholder

--- lzainzinger/app/page.tsx
+++ lzainzinger/app/page.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "@/components/ui/navbar-menue";
 import { BackgroundGradientAnimation } from "../components/ui/background-gradient-animation";
 import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
 import { SparklesCore } from "@/components/ui/sparkles";
+import Link from 'next/link'
+import { cn } from "@/utils/cn";
 
 const words = [
   {
@@ -22,6 +26,55 @@ const words = [
   },
 ];
 
+export function NavbarDemo() {
+  return (
+    <div className="relative w-full flex items-center justify-center">
+      <Navbar className="top-2" />
+      <p className="text-black dark:text-white">
+        The Navbar will show on top of the page
+      </p>
+    </div>
+  );
+}
+
+function Navbar({ className }: { className?: string }) {
+  const [active, setActive] = useState<string | null>(null);
+  return (
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+      <Menu setActive={setActive}>
+        <MenuItem setActive={setActive} active={active} item="Photos">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/photography">Web Development</HoveredLink>
+            <HoveredLink href="/interface-design">Interface Design</HoveredLink>
+            <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
+            <HoveredLink href="/branding">Branding</HoveredLink>
+          </div>
+        </MenuItem>
+        <MenuItem setActive={setActive} active={active} item="Projects">
+          <div className="  text-sm grid grid-cols-2 gap-10 p-4">
+            <ProductItem
+              title="Algochurn"
+              href="/projects"
+              src="/img/main_bg.jpg"
+              description="Prepare for tech interviews like never before."
+            />
+          </div>
+        </MenuItem>
+        <MenuItem setActive={setActive} active={active} item="Pricing">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/hobby">Hobby</HoveredLink>
+            <HoveredLink href="/individual">Individual</HoveredLink>
+            <HoveredLink href="/team">Team</HoveredLink>
+            <HoveredLink href="/enterprise">Enterprise</HoveredLink>
+          </div>
+        </MenuItem>
+      </Menu>
+    </div>
+  );
+}
+
 export default function BackgroundGradientAnimationDemo() {
   return (
     <div className="h-[100vh] relative w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
@@ -36,6 +89,7 @@ export default function BackgroundGradientAnimationDemo() {
         particleColor="#99f6e4"
       />
     </div>
+    <Navbar />
     <h1 className="md:text-7xl text-3xl lg:text-6xl font-bold font-sans hover:font-mono text-center text-white relative z-20">
       Lukas Zainzinger
     </h1>

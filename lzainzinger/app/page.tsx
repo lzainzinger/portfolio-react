5bb2c00191f4154a6e5036f58cd002fd6b1350f9
Zainzinger, Lukas (SI RSS EMEA SV DSC) <lukas.zainzinger@siemens.com>
2024-06-05 11:46:34 +0200

    fix - removed navbar demo export

--- lzainzinger/app/page.tsx
+++ lzainzinger/app/page.tsx
@@ -26,16 +26,6 @@ const words = [
   },
 ];
 
-export function NavbarDemo() {
-  return (
-    <div className="relative w-full flex items-center justify-center">
-      <Navbar className="top-2" />
-      <p className="text-black dark:text-white">
-        The Navbar will show on top of the page
-      </p>
-    </div>
-  );
-}
 
 function Navbar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);

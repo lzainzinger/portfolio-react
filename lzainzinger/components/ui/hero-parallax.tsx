afbb85287f6744e6ab8f30ddc7bcfc68da3b2442
Zainzinger, Lukas (SI RSS EMEA SV DSC) <lukas.zainzinger@siemens.com>
2024-06-06 12:42:24 +0200

    fix - projects page

--- lzainzinger/components/ui/hero-parallax.tsx
+++ lzainzinger/components/ui/hero-parallax.tsx
@@ -31,11 +31,11 @@ export const HeroParallax = ({
   const springConfig = { stiffness: 300, damping: 30, bounce: 100 };
 
   const translateX = useSpring(
-    useTransform(scrollYProgress, [0, 1], [0, -200]),
+    useTransform(scrollYProgress, [0, 1], [0, -100]),
     springConfig
   );
   const translateXReverse = useSpring(
-    useTransform(scrollYProgress, [0, 1], [0, 200]),
+    useTransform(scrollYProgress, [0, 1], [-100, 0]),
     springConfig
   );
   const rotateX = useSpring(
@@ -57,53 +57,41 @@ export const HeroParallax = ({
   return (
     <div
       ref={ref}
-      className="h-[300vh] py-40 overflow-hidden  antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]"
+      className="h-[300vh] w-screen py-10 antialiased [perspective:1000px] [transform-style:preserve-3d]"
     >
       <Header />
-      <motion.div
-        style={{
-          rotateX,
-          rotateZ,
-          translateY,
-          opacity,
-        }}
-        className=""
-      >
-        <motion.div className="flex flex-row-reverse space-x-reverse space-x-20 mb-20">
-          {firstRow.map((product) => (
-            <ProductCard
-              product={product}
-              translate={translateX}
-              key={product.title}
-            />
-          ))}
-        </motion.div>
-        <motion.div className="flex flex-row  mb-20 space-x-20 ">
-          {secondRow.map((product) => (
+      <div className="flex flex-wrap gap-4 justify-between py-10" >
+        {firstRow.map((product) => (
+              <ProductCard
+                product={product}
+                translate={translateX}
+                key={product.title}
+              />
+            ))}
+
+        {secondRow.map((product) => (
             <ProductCard
               product={product}
               translate={translateXReverse}
               key={product.title}
             />
           ))}
-        </motion.div>
-        <motion.div className="flex flex-row-reverse space-x-reverse space-x-20">
-          {thirdRow.map((product) => (
-            <ProductCard
-              product={product}
-              translate={translateX}
-              key={product.title}
-            />
-          ))}
-        </motion.div>
-      </motion.div>
+
+        {thirdRow.map((product) => (
+          <ProductCard
+            product={product}
+            translate={translateX}
+            key={product.title}
+          />
+        ))}
+      </div>
     </div>
   );
 };
 
 export const Header = () => {
   return (
-    <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full  left-0 top-0">
+    <div className="max-w-7xl relative mx-auto p-2 px-4 w-full  left-0 top-0">
       <h1 className="text-2xl md:text-7xl font-bold dark:text-white">
         My Projects
       </h1>
@@ -132,7 +120,7 @@ export const ProductCard = ({
         x: translate,
       }}
       whileHover={{
-        y: -20,
+        y: -10,
       }}
       key={product.title}
       className="group/product h-96 w-[30rem] relative flex-shrink-0"

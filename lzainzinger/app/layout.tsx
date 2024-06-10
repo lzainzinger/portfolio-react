16c97d6d30e5411289a10f72dc0c3d89a59593ea
Zainzinger, Lukas (SI RSS EMEA SV DSC) <lukas.zainzinger@siemens.com>
2024-06-10 11:12:07 +0200

    added vercel web monitor

--- lzainzinger/app/layout.tsx
+++ lzainzinger/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Montserrat } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/ui/header";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 const montserrat = Montserrat({ subsets: ["latin"] })
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={montserrat.className}>
         <Header />
         {children}
+        <Analytics />
       </body>
     </html>
   );
